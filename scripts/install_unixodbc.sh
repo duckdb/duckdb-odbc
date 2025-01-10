deeb30c7ad5f6de80d113a11ce67400272e6a339
Hannes Mühleisen <hannes@muehleisen.org>
2025-01-10 15:51:38 +0100

    dockerrrr

--- scripts/install_unixodbc.sh
+++ scripts/install_unixodbc.sh
@@ -4,7 +4,7 @@ rm -rf build/unixodbc
 mkdir -p build/unixodbc
 cd build/unixodbc
 mkdir sources build
-wget http://www.unixodbc.org/unixODBC-2.3.11.tar.gz || wget http://github.com/lurcher/unixODBC/releases/download/2.3.11/unixODBC-2.3.11.tar.gz
+curl -O -L http://www.unixodbc.org/unixODBC-2.3.11.tar.gz || curl -O -L http://github.com/lurcher/unixODBC/releases/download/2.3.11/unixODBC-2.3.11.tar.gz
 tar -xf unixODBC-2.3.11.tar.gz -C sources --strip-components 1
 cd sources
 ./configure --prefix `cd ../build; pwd` --disable-debug --disable-dependency-tracking --enable-static --enable-gui=no $@
