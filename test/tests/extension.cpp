694eceae2b4dfbd2fc9eb876bc9821e67bf1d382
Maia <maia@duckdblabs.com>
2025-03-19 17:18:11 +0100

    ff

--- test/tests/extension.cpp
+++ test/tests/extension.cpp
@@ -16,17 +16,16 @@ TEST_CASE("Test Extension", "[odbc]") {
 	EXECUTE_AND_CHECK("SQLAllocHandle (HSTMT)", SQLAllocHandle, SQL_HANDLE_STMT, dbc, &hstmt);
 
 	// Install and load the httpfs extension
-	EXECUTE_AND_CHECK("INSTALL httpfs", SQLExecDirect, hstmt,
-	                  ConvertToSQLCHAR("INSTALL httpfs"), SQL_NTS);
+	EXECUTE_AND_CHECK("INSTALL httpfs", SQLExecDirect, hstmt, ConvertToSQLCHAR("INSTALL httpfs"), SQL_NTS);
 
-	EXECUTE_AND_CHECK("LOAD httpfs", SQLExecDirect, hstmt,
-	                  ConvertToSQLCHAR("LOAD httpfs"), SQL_NTS);
+	EXECUTE_AND_CHECK("LOAD httpfs", SQLExecDirect, hstmt, ConvertToSQLCHAR("LOAD httpfs"), SQL_NTS);
 
 	// Check if the extension is loaded
 	EXEC_SQL(hstmt, "SELECT LOADED FROM duckdb_extensions() WHERE extension_name = 'httpfs'");
 
 	// Should return true
-	EXECUTE_AND_CHECK("SQLFetch (SELECT LOADED FROM duckdb_extensions() WHERE extension_name = 'httpfs')", SQLFetch, hstmt);
+	EXECUTE_AND_CHECK("SQLFetch (SELECT LOADED FROM duckdb_extensions() WHERE extension_name = 'httpfs')", SQLFetch,
+	                  hstmt);
 	DATA_CHECK(hstmt, 1, "true");
 
 	// Free the statement handle
