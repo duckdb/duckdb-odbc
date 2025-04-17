f6112255a027b869418e7a7e103ff4f9fee21e71
Alex Kasko <alex@staticlibs.net>
2025-04-17 11:22:42 +0100

    Enable extensions auto-load/install by default
    This change enables `autoload_known_extensions` and
    `autoinstall_known_extensions` options by default the same way it is
    done for release builds of the mainline DuckDB CLI.
    
    Testing: new test is added that checks the auto-loading of the Excel
    extension.
    
    Fixes: #35

--- test/tests/extension.cpp
+++ test/tests/extension.cpp
@@ -36,6 +36,45 @@ TEST_CASE("Test Extension", "[odbc]") {
 	DISCONNECT_FROM_DATABASE(env, dbc);
 }
 
+TEST_CASE("Test extension auto-load using Excel extension", "[odbc]") {
+	SQLHANDLE env;
+	SQLHANDLE dbc;
+
+	HSTMT hstmt = SQL_NULL_HSTMT;
+
+	// Connect to the database using SQLConnect
+	CONNECT_TO_DATABASE(env, dbc);
+
+	// Allocate a statement handle
+	EXECUTE_AND_CHECK("SQLAllocHandle (HSTMT)", SQLAllocHandle, SQL_HANDLE_STMT, dbc, &hstmt);
+
+	// Check that the Excel extension is not yet loaded
+	std::string check_excel_loaded_sql = "SELECT LOADED FROM duckdb_extensions() WHERE extension_name = 'excel'";
+	EXECUTE_AND_CHECK("SQLExecDirect (" + check_excel_loaded_sql + ")", SQLExecDirect, hstmt,
+	                  ConvertToSQLCHAR(check_excel_loaded_sql), SQL_NTS);
+	EXECUTE_AND_CHECK("SQLFetch (" + check_excel_loaded_sql + ")", SQLFetch, hstmt);
+	DATA_CHECK(hstmt, 1, "false");
+
+	// Check whether the Excel extension can be installed and loaded automatically
+	std::string call_excel_text_sql = "SELECT excel_text(1_234_567.897, 'h:mm AM/PM')";
+	EXECUTE_AND_CHECK("SQLExecDirect (" + call_excel_text_sql + ")", SQLExecDirect, hstmt,
+	                  ConvertToSQLCHAR(call_excel_text_sql), SQL_NTS);
+	EXECUTE_AND_CHECK("SQLFetch (" + call_excel_text_sql + ")", SQLFetch, hstmt);
+	DATA_CHECK(hstmt, 1, "9:31 PM");
+
+	// Check that the Excel extension is loaded now
+	EXECUTE_AND_CHECK("SQLExecDirect (" + check_excel_loaded_sql + ")", SQLExecDirect, hstmt,
+	                  ConvertToSQLCHAR(check_excel_loaded_sql), SQL_NTS);
+	EXECUTE_AND_CHECK("SQLFetch (" + check_excel_loaded_sql + ")", SQLFetch, hstmt);
+	DATA_CHECK(hstmt, 1, "true");
+
+	// Free the statement handle
+	EXECUTE_AND_CHECK("SQLFreeStmt (HSTMT)", SQLFreeStmt, hstmt, SQL_CLOSE);
+	EXECUTE_AND_CHECK("SQLFreeHandle (HSTMT)", SQLFreeHandle, SQL_HANDLE_STMT, hstmt);
+
+	DISCONNECT_FROM_DATABASE(env, dbc);
+}
+
 TEST_CASE("Log DuckDB Version", "[odbc]") {
 	SQLHANDLE env;
 	SQLHANDLE dbc;
